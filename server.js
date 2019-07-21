1f5d4a9c884a582093411ffa9d2ef42d38d8e030
chuyloops <alpizarjesus84@gmail.com>
2019-07-20 19:23:09 -0700

    d

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-var expres = required("express");
-var bodyParser = required("body-parser");
+var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
- var PORT = process.env.PORT || 8080;
 
- // For serving of statistic CSS
+var PORT = process.env.PORT || 8000;
 
- app.use(express.static(_dirname = "app/css"));
+// For serving of statics CSS
+app.use(express.static(__dirname = "/express"));
 
- app.use( express)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.app+json" }));
+
+// API and HTML routes 
+require("./app/routing/apiRoutes.js")(app);
+require("./app/roputing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
